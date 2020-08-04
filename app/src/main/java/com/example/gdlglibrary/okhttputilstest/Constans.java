614eb8545bfad83fafc65f37d8b024259b942ddf
PanX <7787489+PanXJin@user.noreply.gitee.com>
2020-08-04 23:39:57 +0800

    Update 8.4 用户链接服务器成功

--- app/src/main/java/com/example/gdlglibrary/okhttputilstest/Constans.java
+++ app/src/main/java/com/example/gdlglibrary/okhttputilstest/Constans.java
@@ -9,5 +9,5 @@ public class Constans {
     //首页路径
     public static String BASE_URL = "http://192.168.0.4:8080/hellossm_war_exploded";
     //登陆路径
-    public static String LOGIN_URL = BASE_URL+"Allusers";
+    public static String LOGIN_URL = BASE_URL+"/Login";
 }
