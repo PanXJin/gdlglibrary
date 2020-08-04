5862cd5f44e572083b538d483e5a6ed6057f37a2
PanX <7787489+PanXJin@user.noreply.gitee.com>
2020-08-04 23:00:08 +0800

    Update 8.4 链接数据库

--- app/src/main/java/com/example/gdlglibrary/okhttputilstest/Constans.java
+++ app/src/main/java/com/example/gdlglibrary/okhttputilstest/Constans.java
@@ -7,5 +7,7 @@ package com.example.gdlglibrary.okhttputilstest;
 public class Constans {
 
     //首页路径
-    public static String LOGIN_URL = "http://192.168.0.4:8080/hellossm_war_exploded/";
+    public static String BASE_URL = "http://192.168.0.4:8080/hellossm_war_exploded";
+    //登陆路径
+    public static String LOGIN_URL = BASE_URL+"Allusers";
 }
